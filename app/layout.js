44f99af3245ec84a29de6a3667519ceeb774411f
Your Name <your.email@example.com>
2025-11-07 21:39:02 +0800

    添加GA

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script'
 import TopNav from '../components/TopNav'
 
 export const metadata = {
@@ -31,6 +32,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YKC544Q00K"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YKC544Q00K');
+          `}
+        </Script>
         <TopNav />
         {children}
       </body>
